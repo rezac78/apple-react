a076abcd28316490fc5196c585f9d14c4b35b815
reza <69678342+rezac78@users.noreply.github.com>
2021-07-26 23:09:53 -0700

    oky

--- src/component/register/Register.jsx
+++ src/component/register/Register.jsx
@@ -1,20 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Register = () => {
+    const [fullname, setFullname] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const reset = () => {
+        setEmail("");
+        setFullname("");
+        setPassword("");
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const user = {
+            fullname,
+            email,
+            password,
+        };
+
+        axios
+            .post('https://toplearnapi.ghorbany.dev/api/register', JSON.stringify(user),
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+            )
+            .then(({ status, data }) => {
+                if (status === 201) {
+                    toast.success('همه چی روبه راهه', { position: "top-right", closeOnClick: true });
+                    console.log(data);
+                    reset();
+                }
+            })
+            .catch(err => {
+                toast.error('خطا رخ داده است', { position: "top-right", closeOnClick: true });
+                reset();
+                console.log(err)
+            })
+
+        console.log(user);
+    }
+
     return (
         <main className="form-signin">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1 className="h3 mb-3 fw-normal">Please Register in</h1>
                 <div className="form-floating">
-                    <input name="fullname" type="text" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                    <input value={fullname} onChange={(e) => setFullname(e.target.value)} name="fullname" type="text" className="form-control" id="floatingInput" placeholder="name@example.com" />
                     <label for="floatingInput">Email address</label>
                 </div>
                 <div className="form-floating">
-                    <input name="email" type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} name="email" type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
                     <label for="floatingInput">Email address</label>
                 </div>
                 <div className="form-floating">
-                    <input name="password" type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} name="password" type="password" className="form-control" id="floatingPassword" placeholder="Password" />
                     <label for="floatingPassword">Password</label>
                 </div>
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
+import {ToastContainer} from "react-toastify";
 import Apple from './Apple';
 
 
@@ -7,6 +8,7 @@ const App=()=>{
     return(
         <BrowserRouter>
             <Apple/>
+            <ToastContainer/>
         </BrowserRouter>
     )
 }
